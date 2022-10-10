9586bb721d5109b71b8c6ee27eedbd79f3a61e76
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-10-10 17:48:46 +0200

    Added ReusableButton

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
         .library(
             name: "civetta",
             targets: ["civetta"]),
+        .library(
+            name: "ReusableButton",
+            targets: ["ReusableButton"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "civetta",
+            dependencies: []),
+        .target(
+            name: "ReusableButton",
             dependencies: []),
         .testTarget(
             name: "civettaTests",
---
+++ Sources/ReusableButton/ReusableButton.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@available(iOS 13.0, macOS 10.15, *)
+public struct ReusableButton: View {
+    public init() {
+        
+    }
+    public var body: some View {
+        Text("Reusable button")
+    }
+}
